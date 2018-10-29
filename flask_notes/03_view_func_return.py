af9f4a47408ff1f64b8b9161671ec94420b4f459
zhuangyongzheng <aczyongz@gmail.com>
2018-10-29 23:40:10 +0800

    整理vim笔记

--- flask_notes/03_view_func_return.py
+++ flask_notes/03_view_func_return.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, make_response
 
 
 app = Flask(__name__)
@@ -9,10 +9,25 @@ app.config.from_object('config')
 
 @app.route('/')
 def index():
-    # 视图函数的return与普通函数的return是不一样的，主要区别在与返回值
-    # 视图函数会返回状态码status-code：200，301，404，503等，含有包含在HTTP header中的content-type
+    # 1.视图函数的return与普通函数的return是不一样的，主要区别在与返回值
+    # 视图函数会返回一个response对象，主要包括状态码status-code：200，301，404，503...，content-type（包含在HTTP header中）等
     # 如果不指定content-type，默认返回为text/html
-    return 'Hello World!'
+    headers = {
+        'content-type': 'text/plain', # json格式 'application/json'
+        'location': 'http://www.baidu.com'
+    }
+    response = make_response('<html></html>', 301) # 状态码只是一个标识，并不会对显示内容有影响
+    response.headers = headers
+    return response
+
+@app.route('/return')
+def return_response():
+    headers = {
+        'content-type': 'text/plain',
+    }
+
+    # 2.这种返回response对象的格式更常用、简洁，
+    return '<html>return</html>', 404, headers
 
 
 if __name__ == '__main__':
